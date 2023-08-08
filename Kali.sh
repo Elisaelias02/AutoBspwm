1acfdb1be9c2ce8d8b031c42c637a05da1a7949e
Cube <116672365+ZLCube@users.noreply.github.com>
2023-08-08 06:14:19 +0000

    Update Kali.sh

--- Kali.sh
+++ Kali.sh
@@ -6,10 +6,6 @@ fi
 
 ruta=$(pwd)
 
-sudo apt update
-
-sudop apt upgrade -y
-
 # Instalando dependencias de Entorno
 
 sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
@@ -29,6 +25,7 @@ sudo apt install -y kitty feh scrot scrub rofi xclip bat locate ranger neofetch 
 # Creando carpeta de Reposistorios
 
 mkdir ~/github
+mkdir ~/ScreenShots
 
 # Descargar Repositorios Necesarios
 
@@ -68,43 +65,20 @@ mkdir -p ~/.config/rofi/themes
 cp $ruta/rofi/nord.rasi ~/.config/rofi/themes/
 
 # Instando lsd
-
 sudo dpkg -i $ruta/lsd.deb
 
-# Instalamos las HackNerdFonts
-
+# Instalamos la fuentes
 sudo cp -v $ruta/fonts/HNF/* /usr/local/share/fonts/
+sudo cp -v $ruta/Fonts2/fonts/* /usr/share/fonts/truetype/
 
-# Instalando Fuentes de Polybar
-
-sudo cp -v $ruta/Config/polybar/fonts/* /usr/share/fonts/truetype/
-
-# Instalando Wallpaper de S4vitar
-
-mkdir ~/Wallpaper
-cp -v $ruta/Wallpaper/* ~/Wallpaper
-mkdir ~/ScreenShots
 
 # Copiando Archivos de Configuración
-
 rm -r ~/.config/polybar
-cp -rv $ruta/Config/* ~/.config/
 sudo cp -rv $ruta/kitty /opt/
-
-# Kitty Root
-
-sudo cp -rv $ruta/Config/kitty /root/.config/
-
-# Copia de configuracion de .p10k.zsh y .zshrc
-
 rm -rf ~/.zshrc
 cp -v $ruta/.zshrc ~/.zshrc
 
-cp -v $ruta/.p10k.zsh ~/.p10k.zsh
-sudo cp -v $ruta/.p10k.zsh-root /root/.p10k.zsh
-
 # Script
-
 sudo cp -v $ruta/scripts/whichSystem.py /usr/local/bin/
 sudo cp -v $ruta/scripts/screenshot /usr/local/bin/
 
@@ -114,37 +88,9 @@ sudo apt install -y zsh-syntax-highlighting zsh-autosuggestions
 sudo mkdir /usr/share/zsh-sudo
 cd /usr/share/zsh-sudo
 sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
-
-# Cambiando de SHELL a zsh
-
 sudo ln -s -fv ~/.zshrc /root/.zshrc
 
-# Asignamos Permisos a los Scritps
-
-chmod +x ~/.config/bspwm/bspwmrc
-chmod +x ~/.config/bspwm/scripts/bspwm_resize
-chmod +x ~/.config/bin/ethernet_status.sh
-chmod +x ~/.config/bin/htb_status.sh
-chmod +x ~/.config/bin/htb_target.sh
-chmod +x ~/.config/polybar/launch.sh
-sudo chmod +x /usr/local/bin/whichSystem.py
-sudo chmod +x /usr/local/bin/screenshot
-
-# Configuramos el Tema de Rofi
-
-rofi-theme-selector
-
-# Instalamos el i3lock
-
-sudo apt install -y i3lock-color
-cd $ruta
-sudo cp -R $ruta/Components/i3lock-color-everblush/i3lock-everblush /usr/bin
-xfconf-query --create -c xfce4-session -p /general/LockCommand -t string -s "i3lock-everblush"
-
-# Removiendo Repositorio
-
-rm -rf ~/github
-rm -rfv $ruta
+./theme.sh
 
 # Mensaje de Instalado
 
